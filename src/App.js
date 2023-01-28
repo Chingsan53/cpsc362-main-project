3da08fd64598d30f5978d2ff929393a092d8db1b
Chingsan53 <cly53@csu.fullerton.edu>
2023-01-28 13:28:25 -0800

    update index

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1 class="text-gray-400 text-3xl">Hello, React!</h1>
+    </div>
+  );
+}
+
+export default App;
